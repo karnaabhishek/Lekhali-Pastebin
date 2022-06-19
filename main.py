e191acb031498d9bb2cb1ad7282473a4bd3287f9
Abhishek Karna <87535625+karnaabhishek@users.noreply.github.com>
2022-06-20 02:19:27 +0545

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ import json
 
 def parser():
     json_data = {}
-    with open('annapurna_news.json', encoding='utf-8') as f:
-        json_data = json.load(f)
+    params = {
+                'page': 0,
+                'title': "कोरोना"
+            }
+    try: 
+        with open('annapurna_news.json', encoding='utf-8') as f:
+            json_data = json.load(f)
+            title = json_data.get('metadata').get('title')
+            page = json_data.get('metadata').get('page') + 1
+    except:
+        json_data["metadata"]=params
+        json.dump(json_data, open('annapurna_news.json', 'w'), indent=4)
         title = json_data.get('metadata').get('title')
         page = json_data.get('metadata').get('page') + 1
     temp_page = page
     while(page<=temp_page+2):
         try:
-            params = {
-                'page': page,
-                'title': title
-            }
+            params['page']=page
+            params['title']=title
             res = requests.get('https://bg.annapurnapost.com/api/search', params=params)
             data = res.json()
             if (res.status_code == 200 and data['status'] == 'success'):
