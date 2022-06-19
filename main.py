80c003bac9adec5aeaefcb504942f5c6867bfcd1
Abhishek Karna <87535625+karnaabhishek@users.noreply.github.com>
2022-06-20 01:11:47 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
+from base64 import encode
+from encodings import utf_8
 import requests
 import json
-import os
 
 def parser():
-    title = 'कोरोना'
-    encoded_title = title.encode('utf-8')
-    # print('encoded title >>', encoded_title)
-    params = {
-    'page': 1,
-    'title': encoded_title
-    }
-    results = {}
-    is_empty = os.stat("annapurna_news.json").st_size == 0
-    if(is_empty):
-        page = 1
-        params["page"]=page
-    else:
-        with open('annapurna_news.json') as f:
-            data = json.load(f)
-            # get the keys from dictionary
-            data_keys_last = int(list(data.keys())[-1])
-            page = data_keys_last + 1
-            params['page'] = page
-            results = data
+    json_data = {}
+    with open('annapurna_news.json', encoding='utf-8') as f:
+        json_data = json.load(f)
+        title = json_data.get('metadata').get('title')
+        page = json_data.get('metadata').get('page') + 1
     temp_page = page
     while(page<=temp_page+2):
-        count=0
         try:
+            params = {
+                'page': page,
+                'title': title
+            }
             res = requests.get('https://bg.annapurnapost.com/api/search', params=params)
             data = res.json()
-
             if (res.status_code == 200 and data['status'] == 'success'):
-                results[page] = data['data']['items']
+                json_data["metadata"]['page'] = params['page']
+                json_data[page] = data['data']['items']
+                json.dump(json_data, open('annapurna_news.json', 'w'), indent=4)
+                print("Fetching page:", page, 'successfully')
                 page = page + 1
                 params['page'] = page
-                json.dump(results, open('annapurna_news.json', 'w'), indent=4)
-            print("Fetching page:", page - 1, 'successfully')
         except Exception as e:
             print(e)
 
-
-parser()
-print("Scraping complete")
+if __name__ == '__main__':
+    parser()
+    print("Scraping complete")
